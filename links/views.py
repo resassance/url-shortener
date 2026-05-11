137d5e2c00a50c1349cb0f35fe8f849f926f5bde
resassance <alieksandr.orlov.03@gmail.com>
2026-05-11 13:38:36 +0500

    Создание ссылок и отображение списка ссылок

--- links/views.py
+++ links/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
-from .forms import RegisterForm
+from .forms import RegisterForm, LinkForm
 from .models import Link
+import uuid
 
 def register_view(request):
     if request.method == 'POST':
@@ -15,7 +16,24 @@ def register_view(request):
     return render(request, 'links/register.html', {'form': form})
 
 def index_view(request):
-    return render(request, 'links/index.html')
+    if request.method == 'POST':
+        form = LinkForm(request.POST)
+        if form.is_valid():
+            link = form.save(commit=False)
+            if request.user.is_authenticated:
+                link.user = request.user
+            
+            link.short_code = str(uuid.uuid4())[:6]
+            link.save()
+            return redirect('index')
+    else:
+        form = LinkForm()
+    
+    links = []
+    if request.user.is_authenticated:
+        links = Link.objects.filter(user=request.user).order_by('-created_at')
+        
+    return render(request, 'links/index.html', {'form': form, 'links': links})
 
 def redirect_view(request, short_code):
     link = get_object_or_404(Link, short_code=short_code)
